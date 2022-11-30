c0eae45f8ae88a449e93d724706100a603155acb
momo2912 <trungdo3224@gmail.com>
2022-11-30 23:16:35 +0700

    init beautiful drag and drop example repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
+import data, { Data } from './initial-data';
+
+
+const grid = 8;
+
+const getItemStyle = (isDragging: any, draggableStyle: any) => ({
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+  background: isDragging ? 'lightgreen' : 'grey',
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver: any) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250,
+});
 
 function App() {
+  const [items, setItems] = useState([] as Data[]);
+  useEffect(() => {
+    setItems(data);
+  }, []);
+
+  const reorder = (list: Array<any>, startIndex: number, endIndex: number) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const onDragEndHandle = (result: any) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const reorderedItems = reorder(
+      items,
+      result.source.index,
+      result.destination.index
+    );
+
+    console.log({ reorderedItems });
+    setItems(reorderedItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_content">
+      <DragDropContext onDragEnd={onDragEndHandle}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      className="card"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
---
+++ src/initial-data.ts
@@ -0,0 +1,45 @@
+export type Data = {
+  id: string;
+  content: string;
+}
+
+const data = [
+  {
+    id: "item-1",
+    content: "Item-1",
+  },
+  {
+    id: "item-2",
+    content: "Item-2",
+  },
+  {
+    id: "item-3",
+    content: "Item-3",
+  },
+  {
+    id: "item-4",
+    content: "Item-4",
+  },
+  {
+    id: "item-5",
+    content: "Item-5",
+  },
+  {
+    id: "item-6",
+    content: "Item-6",
+  },
+  {
+    id: "item-7",
+    content: "Item-7",
+  },
+  {
+    id: "item-8",
+    content: "Item-8",
+  },
+  {
+    id: "item-9",
+    content: "Item-9",
+  },
+];
+
+export default data;
